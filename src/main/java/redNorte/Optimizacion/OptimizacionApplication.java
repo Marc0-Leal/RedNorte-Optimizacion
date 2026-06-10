340093a0ad36b3499fa5b2da46d57e02db9e57b7
niloheri123-ux <niloheri123@gmail.com>
2026-06-09 20:59:26 -0400

    creacion de la api

---
+++ src/main/java/redNorte/Optimizacion/OptimizacionApplication.java
@@ -0,0 +1,13 @@
+package redNorte.Optimizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/redNorte/Optimizacion/OptimizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package redNorte.Optimizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
